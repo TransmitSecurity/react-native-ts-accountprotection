33efde96d77cc3a63e2e0eca0294097e12ff1aaa
Shachar Udi <shachar@transmitsecurity.com>
2024-03-06 12:21:06 -0500

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  TsAccountprotectionExample
+//
+
+import Foundation
---
+++ ios/TsAccountprotection.swift
@@ -0,0 +1,8 @@
+@objc(TsAccountprotection)
+class TsAccountprotection: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}

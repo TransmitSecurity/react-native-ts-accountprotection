00bf988778e938dc3204b8d75266f2fa302bdd2b
Shachar Udi <shachar@transmitsecurity.com>
2024-04-15 12:43:28 -0400

    building test app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -72,7 +72,7 @@ export default class App extends React.Component<Props, State> {
   private onAppReady = async (): Promise<void> => {
     // this is for iOS only, Android TSAccountProtectionSDK is initialized from application onCreate.
     if(Platform.OS === 'ios'){
-      await TSAccountProtectionSDKModule.initializeIOS(config.clientId);
+      await TSAccountProtectionSDKModule.initializeIOS(config.clientId, config.baseUrl);
     }
   }
 

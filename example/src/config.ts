6591ba3e54b835948dcd48147e387775456be11f
Shachar Udi <shachar@transmitsecurity.com>
2024-04-16 09:28:07 -0400

    updated baseurl

--- example/src/config.ts
+++ example/src/config.ts
@@ -1,7 +1,7 @@
 export default {
     clientId: "hcif5tgftwtcz2qld9adddviqusvvij4",
     secret:  "e0711efd-f4e2-4e0d-be91-0ddfe5207347", // Important! This is just for demo purposes. Never store your secret in the client side.
-    baseUrl: "https://api.idsec-stg.com", // eu = "api.eu.transmitsecurity.io" ca = "api.ca.transmitsecurity.io",
+    baseUrl: "https://proxy.riskid-stg.io/device",// "https://api.idsec-stg.com"
     demoUserId: 'user1',
     demoUserPassword: 'user1'
 }

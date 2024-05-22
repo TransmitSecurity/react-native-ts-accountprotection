9b7bfbd47adc0e0ea3600ca224ef241bed7c80fd
Shachar Udi <shachar@transmitsecurity.com>
2024-05-22 13:23:52 +0300

    pause for upgrade

--- example/src/config.ts
+++ example/src/config.ts
@@ -1,7 +1,7 @@
 export default {
     clientId: "hcif5tgftwtcz2qld9adddviqusvvij4",
     secret:  "e0711efd-f4e2-4e0d-be91-0ddfe5207347", // Important! This is just for demo purposes. Never store your secret in the client side.
-    baseUrl: "https://proxy.riskid-stg.io/device",// "https://api.idsec-stg.com"
+    baseUrl: "https://api.idsec-stg.com", //"https://proxy.riskid-stg.io/device",// "https://api.idsec-stg.com"
     demoUserId: 'user1',
     demoUserPassword: 'user1'
 }

33ced0cdf3a4e2c99590cf5680894fe30c139d54
Shachar Udi <shachar@transmitsecurity.com>
2026-01-27 13:40:13 -0500

    MOB-2351 fixed issue with ios mm initializeIOS method

--- ios/TsAccountprotection.mm
+++ ios/TsAccountprotection.mm
@@ -3,7 +3,7 @@
 @interface RCT_EXTERN_MODULE(TsAccountprotection, NSObject)
 
 RCT_EXTERN_METHOD(initializeSDKIOS:(RCTPromiseResolveBlock)resolve withRejecter:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(initializeIOS:(NSString *)clientId baseUrl:(NSString*)baseUrl configurations:(NSDictionary*)configurations withResolver:(RCTPromiseResolveBlock)resolve withRejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(initializeIOS:(NSString *)clientId baseUrl:(NSString*)baseUrl withResolver:(RCTPromiseResolveBlock)resolve withRejecter:(RCTPromiseRejectBlock)reject)
 RCT_EXTERN_METHOD(setUserId:(NSString *)userId withResolver:(RCTPromiseResolveBlock)resolve
                   withRejecter:(RCTPromiseRejectBlock)reject)
 RCT_EXTERN_METHOD(triggerAction:(NSString *)action options:(NSDictionary*)options withResolver:(RCTPromiseResolveBlock)resolve
